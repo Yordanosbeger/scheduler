ead6ad8a00fd5ac80fac2f974f97c0f788fdeb04
Yordanos gebreselasie <yordibege@gmail.com>
2024-01-14 03:15:07 -0700

    add hooks

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,13 @@
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]); 
+
+  function transition(newMode){
+    setHistory(prev => [...prev, newMode])
+  }
+
+  function back() {
+    setHistory(prev => [...prev.slice(0, prev.length - 1)])
+  }
+
+  return { mode: history[history.length -1], transition, back };
+}
