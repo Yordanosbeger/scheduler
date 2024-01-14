13f77df34cf414af387fb919f4b229dab061951b
Yordanos gebreselasie <yordibege@gmail.com>
2024-01-14 04:59:43 -0700

    Reafctor the functions and implement logic

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
+
 export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]); 
 
-  function transition(newMode){
-    setHistory(prev => [...prev, newMode])
-  }
-
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      // Replace the current mode in history with the new mode
+      setHistory(prev => [...prev.slice(0, prev.length - 1), newMode]);
+    } else {
+      // Add the new mode to history
+      setHistory(prev => [...prev, newMode]);
+    }
+  };
   function back() {
     setHistory(prev => [...prev.slice(0, prev.length - 1)])
   }
